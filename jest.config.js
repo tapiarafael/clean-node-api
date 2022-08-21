62a64a9f8b09a1c350f99ed5a6de125162f3f06d
Rafael Tapia <rafaelsilvatapia@gmail.com>
2022-08-21 17:07:12 -0400

    chore: change jest coverage folders

--- jest.config.js
+++ jest.config.js
@@ -5,13 +5,20 @@ module.exports = {
   ],
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/*.ts',
+    '!<rootDir>/src/main/*.ts',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**'
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: 'v8',
+  coverageProvider: 'babel',
 
   // A map from regular expressions to paths to transformers
   transform: {
