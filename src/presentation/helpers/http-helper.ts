d1ca6b22e324ccb7cb442c35ec48a775272e5aae
Rafael Tapia <rafaelsilvatapia@gmail.com>
2022-08-21 16:37:23 -0400

    refactor: move success http response to a helper method

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -10,3 +10,8 @@ export const serverError = (): HttpResponse => ({
   body: new ServerError(),
   statusCode: 500
 })
+
+export const ok = (data: any): HttpResponse => ({
+  body: data,
+  statusCode: 200
+})
