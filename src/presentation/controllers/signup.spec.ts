a797d2896d39c7d04db34814cf6feb0a0bfe4b7b
Rafael Tapia <rafaelsilvatapia@gmail.com>
2022-08-21 12:09:51 -0400

    refactor: inclute tests inside the same describe

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -31,34 +31,34 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
-})
 
-test('Should return 400 if no password is provided', () => {
-  const sut = new SignUpController()
-  const httpRequest = {
-    body: {
-      name: 'any_name',
-      email: 'any_email@email.com',
-      passwordConfirmation: 'any_password'
+  test('Should return 400 if no password is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        passwordConfirmation: 'any_password'
+      }
     }
-  }
 
-  const httpResponse = sut.handle(httpRequest)
-  expect(httpResponse.statusCode).toBe(400)
-  expect(httpResponse.body).toEqual(new MissingParamError('password'))
-})
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
+  })
 
-test('Should return 400 if no password confirmation is provided', () => {
-  const sut = new SignUpController()
-  const httpRequest = {
-    body: {
-      name: 'any_name',
-      email: 'any_email@email.com',
-      password: 'any_password'
+  test('Should return 400 if no password confirmation is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password'
+      }
     }
-  }
 
-  const httpResponse = sut.handle(httpRequest)
-  expect(httpResponse.statusCode).toBe(400)
-  expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
+  })
 })
