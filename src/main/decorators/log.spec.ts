c51c9f79e3c77da08e5e5476914951306819b5be
Rafael Tapia <rafaelsilvatapia@gmail.com>
2022-08-22 16:16:56 -0400

    test: ensure LogControllerDecorator calls controller with correct params

---
+++ src/main/decorators/log.spec.ts
@@ -0,0 +1,26 @@
+import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
+import { LogControllerDecorator } from './log'
+
+describe('LogController Decorator', () => {
+  test('Should call controller handle', async () => {
+    class ControllerStub implements Controller {
+      async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+        const httpResponse: HttpResponse = { statusCode: 200, body: { ok: true } }
+        return await new Promise(resolve => resolve(httpResponse))
+      }
+    }
+    const controllerStub = new ControllerStub()
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+    const sut = new LogControllerDecorator(controllerStub)
+    const httpRequest: HttpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_pass',
+        passwordConfirmation: 'any_pass'
+      }
+    }
+    await sut.handle(httpRequest)
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+})
