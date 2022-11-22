cb4822d66325c0787424027a6979eace6afbf851
Rafael Tapia <rafaelsilvatapia@gmail.com>
2022-11-21 21:19:44 -0400

    test: ensure ValidationComposite works correctly

---
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -0,0 +1,43 @@
+import { MissingParamError } from '../../errors'
+import { Validation } from './validation'
+import { ValidationComposite } from './validation-composite'
+
+interface SutType {
+  sut: ValidationComposite
+  validationStub: Validation
+}
+
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error | undefined {
+      return new MissingParamError('field')
+    }
+  }
+  return new ValidationStub()
+}
+const makeSut = (): SutType => {
+  const validationStub = makeValidation()
+  const sut = new ValidationComposite([validationStub])
+
+  return {
+    sut,
+    validationStub
+  }
+}
+
+describe('ValidationComposite', () => {
+  test('Should return an error if any validation fails', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ field: 'any_value' })
+
+    expect(error).toEqual(new MissingParamError('field'))
+  })
+
+  test('Should not return all validation passes', () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(undefined)
+    const error = sut.validate({ field: 'any_value' })
+
+    expect(error).toBeUndefined()
+  })
+})
