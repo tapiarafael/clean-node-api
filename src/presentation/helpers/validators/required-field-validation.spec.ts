9118b6cfcb5b7c6f9336a48aa3cf844cdc1487bf
Rafael Tapia <rafaelsilvatapia@gmail.com>
2022-11-21 20:57:32 -0400

    refactor:  extract sut creation to a factory functiona

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -1,16 +1,20 @@
 import { MissingParamError } from '../../errors'
 import { RequiredFieldValidation } from './required-field-validation'
 
+const makeSut = (field): RequiredFieldValidation => {
+  return new RequiredFieldValidation(field)
+}
+
 describe('RequiredField Validation', () => {
   test('Should return a MissingParamError if validation fails', () => {
-    const sut = new RequiredFieldValidation('field')
+    const sut = makeSut('field')
     const error = sut.validate({ name: 'any_name' })
 
     expect(error).toEqual(new MissingParamError('field'))
   })
 
   test('Should return undefined if validation succeeds', () => {
-    const sut = new RequiredFieldValidation('name')
+    const sut = makeSut('name')
     const error = sut.validate({ name: 'any_name' })
 
     expect(error).toBeUndefined()
